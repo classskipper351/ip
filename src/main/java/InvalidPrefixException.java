bd6b37fa6997564a96b0706ad5bd53f0c4f948a3
晨珖 姚 <1014717807@QQ.com>
2024-02-14 23:54:24 +0800

    add error handlling by throwing exceptions

---
+++ src/main/java/InvalidPrefixException.java
@@ -0,0 +1,5 @@
+public class QchatException extends Exception{
+
+
+
+}
--- src/main/java/Qchat.java
+++ src/main/java/Qchat.java
@@ -3,41 +3,33 @@ import java.util.Scanner;
 
 public class Qchat {
 
-   static final String  WELCOME_GREETING = new String(
-           "Qchat ,A truly humanized intelligent voice assisstant \n"
+   static final String  WELCOME_GREETING =
+           "Qchat ,A truly humanized intelligent voice assistant \n"
                    +"knows better about life and better about you\n"
                    +"What can I do for you?\n"
-                   +"----------------------------------------------------------------\n") ;
-   static final String GOODBYE_GREETING = new String(
+                   +"----------------------------------------------------------------\n" ;
+   static final String GOODBYE_GREETING =
            "--------------------------------------------------------\n" +
-            "goodbye\n"+ "Qchat, your life-long trusted companion\n");
-
-
+            "goodbye\n"+ "Qchat, your life-long trusted companion\n";
 
 
    static String CommandReader(){
-
        Scanner in = new Scanner(System.in);
        String command ;
        command = in.nextLine();
-
-       switch (command){
-       case "Bye":
-           System.out.print(GOODBYE_GREETING);
-           break;
-       case "list":
-           Todolist.HandleList();
-           break;
-
-
-       default:
-           echo(command);
-           break;
-
-       }
+           switch (command) {
+           case "Bye":
+               System.out.print(GOODBYE_GREETING);
+               break;
+           case "list":
+               ListManager.HandleList();
+               break;
+           default:
+               echo(command);
+               break;
+           }
+       //in.close();
        return command;
-
-
 
    }
 
@@ -48,13 +40,10 @@ public class Qchat {
     }
 
     public static void main(String[] args) {
-
        System.out.print(WELCOME_GREETING);
        String command = "" ;
        while(!command.equals("Bye")){
-
            command = CommandReader();
-
        }
 
    }
